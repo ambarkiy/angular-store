df117b34b356c2f1cbc54bab80e1276516181dd2
yambarki-ext@groupe-bel.com <YAMBARKI-EXT@groupe-bel.com>
2020-12-08 09:10:31 +0100

    Exercice : Composant TopNavigation
    1.	Créer le composant TopNavigation à partir de la cli et le modifier pour correspondre à la maquette

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TopNavigationComponent } from './top-navigation/top-navigation.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopNavigationComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
