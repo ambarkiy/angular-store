c563d6fa7bccfbd69bdbf463f827d9e593d85230
yambarki-ext@groupe-bel.com <YAMBARKI-EXT@groupe-bel.com>
2020-12-13 11:20:26 +0100

    Exercice-4.1: - ajout de l'input oubli - afficher un article sur la page d'accueil

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ImgRegisterService } from './common';
 import { Link, Store, StoreService } from './domain';
+import { StoreItem } from './domain/models';
 
 @Component({
   selector: 'app-root',
@@ -11,6 +12,7 @@ export class AppComponent implements OnInit {
   store: Store;
   links: Link[];
   color: string;
+  storeItem: StoreItem;
 
   // TODO: remove dependency to ImgRegisterService
   constructor(private storeService: StoreService, private imgRegisterService: ImgRegisterService) {
@@ -21,6 +23,7 @@ export class AppComponent implements OnInit {
     this.store = this.storeService.get();
     this.color = 'primary';
     this.links = this.storeService.getFooterLinks();
+    this.storeItem = { name: 'Végétarienne', price: 10, description: 'Base tomate, mozzarella, champignons frais, oignons frais, poivrons, tomates fraîches', img: '/assets/images/store/pizzas-vegetarienne.png' };
   }
 
 }
--- src/app/store-item/store-item.component.ts
+++ src/app/store-item/store-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { StoreItem } from '../domain/models';
 
 @Component({
   selector: 'app-store-item',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StoreItemComponent implements OnInit {
 
+  @Input() storeItem: StoreItem;
   constructor() { }
 
   ngOnInit(): void {
