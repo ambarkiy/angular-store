bd66f66db2e00955d92eff52ccbe8c1602c8d906
yambarki-ext@groupe-bel.com <YAMBARKI-EXT@groupe-bel.com>
2020-12-26 11:14:59 +0100

    Exercice-8: Modifier les éléments de la vitrine - ajouts globaux des package json-server et npm-run-all - ajout d'un nouveaux script pour lancer en paralèlle le front et le server - modifications du service store-service pour utiliser les api

--- src/app/domain/services/store.service.ts
+++ src/app/domain/services/store.service.ts
@@ -1,8 +1,11 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 
 import { LINKS, STORE, STORE_ITEMS } from '../data';
 import { Link, Store, StoreItem } from '../models';
+
+const API_URL = "http://localhost:4000/store";
 
 
 @Injectable({
@@ -12,7 +15,10 @@ export class StoreService {
 
   _storeBS = new BehaviorSubject<Store>(STORE);
 
-  constructor() {
+  constructor(private httpClient: HttpClient) {
+    this.httpClient.get<Store>(API_URL).toPromise().then(store => {
+      this._storeBS.next(store);
+    });
   }
 
   get(): Observable<Store> {
@@ -28,6 +34,9 @@ export class StoreService {
   }
 
   update(store: Store) {
-    this._storeBS.next(store);
+    this.httpClient.put<Store>(API_URL, store).toPromise().then(() => {
+      this._storeBS.next(store);
+    });
+
   }
 }
